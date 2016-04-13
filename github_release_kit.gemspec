fb9764f427cab915aab7ba88799f2f9f7499ad18
Keisuke Ogaki <keisuke_ogaki@dwango.co.jp>
2016-04-14 01:14:04 +0900

    :star: release command

--- github_release_kit.gemspec
+++ github_release_kit.gemspec
@@ -27,6 +27,8 @@ Gem::Specification.new do |spec|
   spec.require_paths = ["lib"]
 
   spec.add_dependency "thor"
+  spec.add_dependency "mime-types"
+  spec.add_dependency "octokit", "~> 4.0"
   spec.add_development_dependency "bundler", "~> 1.11"
   spec.add_development_dependency "rake", "~> 10.0"
 end
--- lib/github_release_kit/cli.rb
+++ lib/github_release_kit/cli.rb
@@ -1,10 +1,34 @@
 require 'thor'
+require 'octokit'
 
 module GithubReleaseKit
   class CLI < Thor
-    desc "github_release_kit", "release kit"
-    def release
-      p "hello"
+    class_option :token, :type => :string, :required => true
+    class_option :user, :type => :string, :required => true
+    class_option :api_url, :type => :string, :default => "https://api.github.com"
+    desc "github_release_kit REPOSITORY_URL FILEPATH", "release kit"
+    def release(repository_url, filepath)
+      filename = File.basename(filepath)
+      init
+
+      repository = Octokit::Repository.from_url(repository_url)
+      releases = Octokit.releases(repository)
+      nightly_release = releases.select{|repo| repo.tag_name == "nightly"}.first
+      nightly_release_assets = Octokit.release_assets(nightly_release.url)
+      existing_asset = nightly_release_assets.select{|asset| asset.name == filename}.first
+      unless existing_asset.nil?
+        Octokit.delete_release_asset(existing_asset.url)
+      end
+      Octokit.upload_asset(nightly_release.url, filepath)
+    end
+
+    private
+    def init
+      Octokit.configure do |c|
+        c.api_endpoint = options[:api_url]
+        c.login = options[:user]
+        c.password = options[:token]
+      end
     end
   end
 end
