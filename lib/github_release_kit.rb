742528d173f181a7e92d0733f8754f52d3719050
Keisuke Ogaki <keisuke_ogaki@dwango.co.jp>
2016-04-14 00:16:11 +0900

    cli

--- lib/github_release_kit.rb
+++ lib/github_release_kit.rb
@@ -1,4 +1,5 @@
 require "github_release_kit/version"
+require "github_release_kit/cli"
 
 module GithubReleaseKit
   # Your code goes here...
--- lib/github_release_kit/cli.rb
+++ lib/github_release_kit/cli.rb
@@ -1,5 +1,10 @@
+require 'thor'
+
 module GithubReleaseKit
   class CLI < Thor
-
+    desc "github_release_kit", "release kit"
+    def release
+      p "hello"
+    end
   end
 end
